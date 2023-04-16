073b3ca0131ecf88a3f477017046cf4d8eddc18d
KaisarBaimukhambet <123915283+KaisarBaimukhambet@users.noreply.github.com>
2023-04-16 12:25:45 +0600

    Update lab8.go

--- lab8.go
+++ lab8.go
@@ -3,16 +3,16 @@ package main
 import (
     "fmt"
     "net/http"
-)
+)//Импортируем пакет net/http для создания сервера
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Hello, World!")
-}
+}//http.ResponseWriter используется для отправки ответа клиенту
 
 func main() {
-    mux := http.NewServeMux()
+    mux := http.NewServeMux()//содает новый роутер
     mux.HandleFunc("/", rootHandler)
 
     fmt.Println("Server listening on port 8000")
-    http.ListenAndServe(":8000", mux)
+    http.ListenAndServe(":8000", mux)// запускаем сервер на порту 8000 и используем мультиплексный маршрутизатор для обработки входящих запросов
 }

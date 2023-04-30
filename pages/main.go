1b87f9e61048be7830ef03245b81fcd1b11432b9
KaisarBaimukhambet <123915283+KaisarBaimukhambet@users.noreply.github.com>
2023-04-30 17:48:57 +0600

    Update main.go

--- pages/main.go
+++ pages/main.go
@@ -1,13 +1,40 @@
 package main
 
 import (
+    "database/sql"
     "fmt"
     "log"
     "net/http"
+
+    _ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
+    // Create a new database file and connect to it
+    db, err := sql.Open("sqlite3", "./example.db")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer db.Close()
+
+    // Create a table for form submissions
+    _, err = db.Exec(`
+        CREATE TABLE IF NOT EXISTS form_submissions (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            email TEXT,
+            phone TEXT,
+            message TEXT
+        )
+    `)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // Register the form submission handler
     http.HandleFunc("/", handleFormSubmission)
+
+    // Start the server
     log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
@@ -17,6 +44,21 @@ func handleFormSubmission(w http.ResponseWriter, r *http.Request) {
     email := r.FormValue("email")
     phone := r.FormValue("phone")
     message := r.FormValue("message")
+
+    // Insert the form data into the database
+    db, err := sql.Open("sqlite3", "./example.db")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer db.Close()
+
+    _, err = db.Exec(`
+        INSERT INTO form_submissions (name, email, phone, message)
+        VALUES (?, ?, ?, ?)
+    `, name, email, phone, message)
+    if err != nil {
+        log.Fatal(err)
+    }
 
     // Print the form data to the console
     fmt.Println("Name:", name)
